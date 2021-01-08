f59740b26093aa95bc110efa6205e890b4b8476a
Joshua Russell <joshuarussell@me.com>
2021-01-09 02:47:19 +1100

    feat: added data binding example

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="bg-black p-12 text-center border-b border-gray-500">
     <h1 class="text-white font-bold text-4xl">{{ fields.title }}</h1>
-    <p class="text-white">{{ fields.description }}</p>
+    <p class="text-white mb-6">{{ fields.description }}</p>
+    <button
+      @click="buttonClick"
+      class="bg-white rounded py-3 p-6 text-black mb-6"
+    >
+      Click Me
+    </button>
+    <p class="text-white">Button clicked {{ counter }} times</p>
   </section>
 </template>
 
@@ -12,6 +19,16 @@ export default {
       type: Object,
       required: false,
       default: () => {},
+    },
+  },
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    buttonClick() {
+      this.counter++
     },
   },
 }
